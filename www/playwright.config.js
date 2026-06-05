a69991d45cd0319e5212f5df02704e8c48a432fa
Juan Diego Quintero Z <quinterozambranojuandiego@gmail.com>
2026-06-05 17:40:11 -0500

    Ajustes para las pruebas PIPELINE

--- www/playwright.config.js
+++ www/playwright.config.js
@@ -1,4 +1,3 @@
-// playwright.config.js
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
@@ -7,5 +6,11 @@ export default defineConfig({
     baseURL: 'http://localhost:3000',
     headless: true,
     screenshot: 'only-on-failure',
+  },
+  webServer: {
+    command: 'npx serve . -p 3000',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+    timeout: 15000,
   },
 });
